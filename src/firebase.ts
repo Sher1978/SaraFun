5a2638783bc6d96e53a8a14d69cb33ebe61b08bc
Igor Sherlock <sherlock@sarafun.site>
2026-03-06 17:41:50 +0700

    Firebase: Fix connection config, deploy rules and indexes (Sprint 25)

--- src/firebase.ts
+++ src/firebase.ts
@@ -2,12 +2,12 @@ import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-    apiKey: "YOUR_API_KEY",
-    authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
-    projectId: "YOUR_PROJECT_ID",
-    storageBucket: "YOUR_PROJECT_ID.appspot.com",
-    messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-    appId: "YOUR_APP_ID"
+    apiKey: "AIzaSyBIxNIZLHnfXGogTMkkHjyeomjPNsTYIzE",
+    authDomain: "sarafun-777.firebaseapp.com",
+    projectId: "sarafun-777",
+    storageBucket: "sarafun-777.firebasestorage.app",
+    messagingSenderId: "994908147296",
+    appId: "1:994908147296:web:b3751a43bc169efff9fd05"
 };
 
 const app = initializeApp(firebaseConfig);
